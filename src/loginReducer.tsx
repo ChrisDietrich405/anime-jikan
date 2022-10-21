57eb404c2828f3688e3d98038ed7e863968e46b5
Chris <chrisdietrich3666@gmail.com>
2022-10-21 12:29:34 -0500

    feat added login functionality

--- src/loginReducer.tsx
+++ src/loginReducer.tsx
@@ -1,11 +1,36 @@
+interface IInitialState {
+  password: string;
+  username: string;
+  error: string;
+  isLoggedIn: boolean;
+  isLoading: boolean;
+}
+
+interface IAction {
+  type: string;
+  payload: any;
+  fieldName: string;
+}
+
 export const initialState = {
   password: "",
   username: "",
   error: "",
   isLoggedIn: "",
-  isLoading: ""
-}
+  isLoading: "",
+};
 
-export const loginReducer = (state, action) => {
-
-}
+export const loginReducer = (state: IInitialState, action: IAction) => {
+  switch (action.type) {
+    case "field":
+      return { ...state, [action.fieldName]: action.payload };
+    case "login":
+      return { ...state, isLoading: true, error: "" };
+    case "success":
+      return { ...state, isLoggedIn: true, isLoading: false };
+    case "error":
+      return { ...state, error: "big problems", username: "", password: "" };
+    default:
+      return state;
+  }
+};
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,44 @@
-import React from 'react'
+import React, { useReducer } from "react";
+import { initialState, loginReducer } from "../../loginReducer";
+import { login } from "../../utils";
 
 const Login = () => {
+  const [state, dispatch] = useReducer(loginReducer, initialState);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "login" });
+    try {
+      await login();
+      dispatch({ type: "success" });
+    } catch (error) {
+      dispatch({ type: "error" });
+    }
+  };
   return (
     <div>
-      <form>
-        <input type="text" placeholder="username" />
-        <input type="text" placeholder="password" />
-      </form>
+      {loggedIn ? (
+        <>
+          <h1>Welcome {username}</h1>
+          <button onClick={() => dispatch({ type: "error" })}>Log out</button>
+        </>
+      ) : (
+        <form onSubmit={onSubmit}>
+          <input type="text" placeholder="username" onChange={(e) => dispatch({
+            type: "field",
+            payload: e.target.value
+            fieldName: "username"
+          })}/>
+          <input type="text" placeholder="password" onChange={(e) => dispatch({
+            type: "field",
+            payload: e.target.value,
+            fieldName: "password"
+          })}/>
+          <button type="submit">login</button>
+        </form>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
